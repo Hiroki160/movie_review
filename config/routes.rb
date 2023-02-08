4b398589a717f0031964d42080182ee18e4cd8a4
EC2 Default User <sahiyuno0934@gmail.com>
2023-02-08 10:09:39 +0000

    ユーザー詳細画面とヘッダーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: 'users#show'
+
   devise_for :users
   get '/' => "homes#top"
+  
+  resource :user, except: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
