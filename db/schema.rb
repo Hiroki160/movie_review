4138ac03d5ffae2f007fd6b794ca86eb0cf06e73
EC2 Default User <sahiyuno0934@gmail.com>
2023-02-24 10:24:26 +0000

    いいねモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_10_230945) do
+ActiveRecord::Schema.define(version: 2023_02_24_102311) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "movie_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
