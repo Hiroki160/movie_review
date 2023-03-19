240f09dfa2f6ca09887b758740095e007b871ecb
EC2 Default User <sahiyuno0934@gmail.com>
2023-03-19 01:23:06 +0000

    アバウトページ作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,16 @@
 class ReviewsController < ApplicationController
   def index
   end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to ramen_shop_reviews_path(@review.movie)
+    else
+      @movie = Movie.find(params[:movie_id])
+      render "movies/show"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   root to: 'users#show'
 
   devise_for :users
-  get '/' => "homes#top"
+  get 'homes/top' => "homes#top"
 
   resource :user, except: [:new, :create, :destroy]
   resources :movies do
-    resource :favorites, only: [:create, :destroy] 
+    resource :favorites, only: [:create, :destroy]
     resources :reviews, only: [:index, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
